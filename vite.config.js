eee5ddbba5147e320855a94fc7078080cd8ebee8
Raraselz <raresmihail08@gmail.com>
2025-11-25 23:39:50 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // Use a relative base so built files work when served from GitHub Pages
-  base: './',
+  base: '/drignei-web/',
   build: {
     outDir: 'docs',
   },
   plugins: [react(), tailwindcss()],
-  base: '/drignei-web/'
 })
