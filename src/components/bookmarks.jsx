fd1ecffe659a06045ce5fcf815aeceead943eea1
unknown <costidimian@gmail.com>
2025-11-26 09:10:57 +0200

    .

--- src/components/bookmarks.jsx
+++ src/components/bookmarks.jsx
@@ -14,11 +14,13 @@ function Bookmarks() {
         continue;
       }
       else{
-        newBookmarks.push({
+        const bookmark = {
           href: `https://www.facebook.com/maria.drignei/?locale=ro_RO`,
           src: `https://scontent.fcra1-1.fna.fbcdn.net/v/t39.30808-1/356379865_3457504497848826_2587823873892593405_n.jpg?stp=c0.0.396.396a_dst-jpg_s200x200_tt6&_nc_cat=107&ccb=1-7&_nc_sid=e99d92&_nc_ohc=nVCbaWUGeuAQ7kNvwFCb4Ea&_nc_oc=AdmvycPxNoB1VoPx_C9MGRKSoetcHY-ni5f2CDFKvI1YtIykPqNtR6_BXIq4VXBJI60&_nc_zt=24&_nc_ht=scontent.fcra1-1.fna&_nc_gid=NpuwEgKPXo1e6oqhs9Yrgg&oh=00_AfhS8Bt-1j_b0CjZGZF559eCYkFhY80Wkc76bYBUJ6mhlg&oe=692BF391`,  
           alt: `Bookmark ${i}`
-        });
+        }
+        newBookmarks.push(bookmark);
+        localStorage.setItem('bookmark_' + i, JSON.stringify(bookmark));
       }
       // const bookmark = {
       //   href: `https://www.facebook.com/maria.drignei/?locale=ro_RO`,
