f0fa2a4da56a2796fa3f43a484a633eab315736c
constantyn-silvian <costidimian@gmail.com>
2025-11-29 22:44:52 +0200

    merge time with optimisation

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -37,14 +37,17 @@ export default function SideBar() {
           className="ml-4 font-semibold hover:font-bold"
           to="/drignei-web/bouncegame"
         >
-          Bounce Game
+          Cosmin Bounce
         </Link>
       </div>
 
       <div
         aria-hidden={false}
-        className="absolute left-1/2 transform -translate-x-1/2 text-white font-light text-lg"
-        style={{ pointerEvents: 'none' }}
+        className="absolute text-center text-lg left-1/2 transform -translate-x-1/2 text-white font-light "
+        style={{ pointerEvents: 'none',
+          fontSize: window.innerWidth > 900 ? "1.25rem" : "0.75rem",
+          width: '20vw'
+         }}
       >
         {currentTimeText}
       </div>
