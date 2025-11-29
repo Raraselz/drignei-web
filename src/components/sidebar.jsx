aa10535ca12a8fa9e89784f4c8e16b6eee75286b
Raraselz <raresmihail08@gmail.com>
2025-11-29 22:16:57 +0200

    add build

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -10,7 +10,7 @@ export default function SideBar() {
       const hours = String(now.getHours()).padStart(2, '0')
       const minutes = String(now.getMinutes()).padStart(2, '0')
       const seconds = String(now.getSeconds()).padStart(2, '0')
-      setCurrentTimeText(`${hours}:${minutes}:${seconds}`)
+      setCurrentTimeText(`${hours}:${minutes}:${seconds} ${now.toDateString()}`)
     }
 
     update()
